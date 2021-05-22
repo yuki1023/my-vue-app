0f515d8553e7548246556a6844dd1750d95a98e9
樋口裕貴 <sai102343@gmail.com>
2021-05-22 14:37:06 +0900

    HelloWorld.Vueを削除

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="hello-wrold">
+      <img alt="Vue logo" src="./assets/logo.png" />
+      <div class="hello-wrold__message">
+        {{ message }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue"
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "WebExpert Course Vue Template",
+    }
   },
+  methods: {},
 }
 </script>
 
@@ -21,8 +26,16 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+}
+</style>
+
+<style scoped>
+.hello-wrold {
   margin-top: 60px;
+  text-align: center;
+}
+
+.hello-wrold .hello-world__message {
+  color: #2c3e50;
 }
 </style>
