0d2b3065fc2955411b1d7b866803602fc6d6b0dc
mando-narin <y2pgftpe@keio.jp>
2021-05-22 15:42:23 +0900

    Counter.vueを実装

---
+++ src/components/Counter.vue
@@ -0,0 +1,30 @@
+<!-- ❗️ .vueファイルの中で、<template>タグは、1つの子しか持てません。❗️ -->
+<template>
+  <div class="counter">{{ count }}</div>
+</template>
+
+<script>
+export default {
+  props: {
+    initialValue: {
+      type: Number,
+      default: 0,
+    },
+  },
+  data: function () {
+    return {
+      count: this.initialValue,
+    }
+  },
+}
+</script>
+
+<!-- ❗️ <style scoped>を使うと、CSSのスコープをこの.vueファイルに限定できます! ❗️ -->
+<style scoped>
+.counter {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: lightgreen;
+}
+</style>
